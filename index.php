01070854c756b375f97f493077eae1d581d7728a
Rachana Gaire <rachnagaire12@gmail.com>
2024-11-17 10:11:42 -0600

    fixes

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php include 'header.php'; ?>
     
-<<<<<<< HEAD
 <main role="main">
         <section class="hero-section hero--bg-pattern">
             <div class="container">
@@ -101,39 +100,4 @@
             </div>
         </section>
     </main>
-=======
-<div class="container">
-    <div class="row">
-        <div class="col d-flex align-item-center justify-content-center">
-            <div class="card w-25 m-3">
-                <div class="card-body p-4 bg-primary">
-                    <h5 class="card-title">Admin</h5>
-                    <a href="#" class="btn btn-primary" data-mdb-ripple-init>Button</a>
-                </div>
-            </div>
-            <div class="card w-25 m-3">
-                <div class="card-body p-4 bg-primary">
-                    <h5 class="card-title">Doctor</h5>
-                    <a href="#" class="btn btn-primary" data-mdb-ripple-init>Button</a>
-                </div>
-            </div>
-            <div class="card w-25 m-3">
-                <div class="card-body p-4 bg-primary">
-                    <h5 class="card-title">Patient</h5>
-                    <a href="#" class="btn btn-primary" data-mdb-ripple-init>Button</a>
-                </div>
-            </div>
-            
-
-
-           
-        </div>
-    </div>
-    <div class="row">
-        <div class="col">
-         <h2><a href="./PatientsInfo.php">Patients Details</a></h2>
-        </div>
-    </div>
-</div>
->>>>>>> ceaf0d168c6562778983b733f3e5e18f2dbef745
 <?php include 'footer.php';?>
